3ed6aaa530ec1d5de9013edd823f29fb97e9f5e7
michaelerb <michael.p.erb@gmail.com>
2021-10-04 14:33:06 -0700

    Pushing the most recent version

--- run_da.sh
+++ run_da.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=da_Holocene        # Name of the job
 #SBATCH --output=output_logfile.out   # File for output and errors
-#SBATCH --time=3-00:00:00             # Maximum time for job to run
+#SBATCH --time=6:00:00                # Maximum time for job to run
 #SBATCH --mem=30000                   # Memory (MB)
 #SBATCH --cpus-per-task=1             # Number of processors
 
